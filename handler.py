0bbe00fad2da3e9dbb73050b085d4c38d386ed44
stephane soulier <stephanes@prettysimplegames.com>
2018-07-12 22:09:11 +0200

    avert the user if he try to copy/paste several frichti commands Wait for status code 200 for callback

--- handler.py
+++ handler.py
@@ -370,45 +370,51 @@ def handler(event, _):
 
             r = requests.post(payload["response_url"], json=callback_result)
 
+            if r.status_code == 200:
+                return
+
     else:
         user_id = body['user_id'][0]
         print(user_id)
         user_name = body['user_name'][0]
         command = body['text'][0]
-        args = command.split(' ')
-
-        action = args[0]
-        if (action in AVAILABLE_COMMANDS):
-
-            if action in PLATE_TYPES:
-                if len(args) > 1:
-                    plates = ' '.join(args[1:]).split(';')
-                    plates = [item.strip() for item in plates]
-                else:
-                    plates = []
-
-                result = order(user_id, user_name, action, *plates)
-            elif action == "edit":
-                result = edit(user_id)
-            elif action == "clear":
-                result = remove()
-            elif action == "list":
-                if len(args) > 1:
-                    if args[1] == 'team':
-                        result = list_team()
-                    else:
-                        result = {
-                            'text': 'This is a not valid command for `list`. Use `/frichti list` for your order or `/frichti list team` for the team oder.'}
-                else:
-                    result = list_user(user_id)
-            elif action == "order":
-                result = list_aggregate()
-            else:
-                result = {"text": "This feature is not yet implemented :grimacing:"}
+        if '/frichti' in command:
+            result = {"text": "You probably copy/pasted 2 or more `/frichti` commands. Please use it one by one :wink:"}
         else:
+            args = command.split(' ')
 
-            result = {
-                "text": "This command doesn't exist :grimacing:. Use `starter | main | dessert | extra | edit | list`"}
+            action = args[0]
+            if (action in AVAILABLE_COMMANDS):
+
+                if action in PLATE_TYPES:
+                    if len(args) > 1:
+                        plates = ' '.join(args[1:]).split(';')
+                        plates = [item.strip() for item in plates]
+                    else:
+                        plates = []
+
+                    result = order(user_id, user_name, action, *plates)
+                elif action == "edit":
+                    result = edit(user_id)
+                elif action == "clear":
+                    result = remove()
+                elif action == "list":
+                    if len(args) > 1:
+                        if args[1] == 'team':
+                            result = list_team()
+                        else:
+                            result = {
+                                'text': 'This is a not valid command for `list`. Use `/frichti list` for your order or `/frichti list team` for the team oder.'}
+                    else:
+                        result = list_user(user_id)
+                elif action == "order":
+                    result = list_aggregate()
+                else:
+                    result = {"text": "This feature is not yet implemented :grimacing:"}
+            else:
+
+                result = {
+                    "text": "This command doesn't exist :grimacing:. Use `starter | main | dessert | extra | edit | list`"}
 
         response = responseTemplate.copy()
         response['body'] = json.dumps(result)
@@ -417,38 +423,11 @@ def handler(event, _):
 
 
 if __name__ == '__main__':
-    event = {'resource': '/frichti', 'path': '/frichti', 'httpMethod': 'POST',
-             'headers': {'Accept': 'application/json,*/*', 'Accept-Encoding': 'gzip,deflate',
-                         'CloudFront-Forwarded-Proto': 'https', 'CloudFront-Is-Desktop-Viewer': 'true',
-                         'CloudFront-Is-Mobile-Viewer': 'false', 'CloudFront-Is-SmartTV-Viewer': 'false',
-                         'CloudFront-Is-Tablet-Viewer': 'false', 'CloudFront-Viewer-Country': 'US',
-                         'Content-Type': 'application/x-www-form-urlencoded',
-                         'Host': 'cwpc4sd08l.execute-api.us-east-1.amazonaws.com',
-                         'User-Agent': 'Slackbot 1.0 (+https://api.slack.com/robots)',
-                         'Via': '1.1 78ae32a88b9156d6c12be8f261f1c1b8.cloudfront.net (CloudFront)',
-                         'X-Amz-Cf-Id': 'PJCM62crYbWZYFIhdy-q_OLUC4Ypi1L4WrJ6sWcWNnxpmpkThsg_VA==',
-                         'X-Amzn-Trace-Id': 'Root=1-5b426d8c-17355f75e36a8a980455658e',
-                         'X-Forwarded-For': '52.71.192.87, 54.182.230.66', 'X-Forwarded-Port': '443',
-                         'X-Forwarded-Proto': 'https', 'X-Slack-Request-Timestamp': '1531080076',
-                         'X-Slack-Signature': 'v0=018d0453b72947387b0eb1cc7d76b8004b048d1d9fe83d6f04b8f3fa50e61bf8'},
-             'queryStringParameters': None, 'pathParameters': None, 'stageVariables': None,
-             'requestContext': {'resourceId': '1n9979', 'resourcePath': '/frichti', 'httpMethod': 'POST',
-                                'extendedRequestId': 'JuYOAH5WoAMFpHQ=', 'requestTime': '08/Jul/2018:20:01:16 +0000',
-                                'path': '/dev/frichti', 'accountId': '483879061606', 'protocol': 'HTTP/1.1',
-                                'stage': 'dev', 'requestTimeEpoch': 1531080076830,
-                                'requestId': 'ac868cef-82e9-11e8-ad92-cb54d0a9fa39',
-                                'identity': {'cognitoIdentityPoolId': None, 'accountId': None,
-                                             'cognitoIdentityId': None, 'caller': None, 'sourceIp': '52.71.192.87',
-                                             'accessKey': None, 'cognitoAuthenticationType': None,
-                                             'cognitoAuthenticationProvider': None, 'userArn': None,
-                                             'userAgent': 'Slackbot 1.0 (+https://api.slack.com/robots)', 'user': None},
-                                'apiId': 'cwpc4sd08l'},
-             'body': 'payload=%7B%22type%22%3A%22interactive_message%22%2C%22actions%22%3A%5B%7B%22name%22%3A%22remove%22%2C%22type%22%3A%22button%22%2C%22value%22%3A%22%7B%5C%22plate_type%5C%22%3A+%5C%22Main%5C%22%2C+%5C%22plate%5C%22%3A+%5C%22crev%5C%5Cu00e8te+%5C%5Cu00e0+l%5C%5Cu2019ancienne%5C%22%7D%22%7D%5D%2C%22callback_id%22%3A%22edit%22%2C%22team%22%3A%7B%22id%22%3A%22TAD1PA6TX%22%2C%22domain%22%3A%22pussycratie%22%7D%2C%22channel%22%3A%7B%22id%22%3A%22CAC6KM5K2%22%2C%22name%22%3A%22general%22%7D%2C%22user%22%3A%7B%22id%22%3A%22UADUGGKAB%22%2C%22name%22%3A%22stephane%22%7D%2C%22action_ts%22%3A%221531080076.747641%22%2C%22message_ts%22%3A%221531080058.000074%22%2C%22attachment_id%22%3A%221%22%2C%22token%22%3A%22j96WF0mOCsNFaPBninbGyUnX%22%2C%22is_app_unfurl%22%3Afalse%2C%22response_url%22%3A%22https%3A%5C%2F%5C%2Fhooks.slack.com%5C%2Factions%5C%2FTAD1PA6TX%5C%2F395009340709%5C%2FDO7qJjIktZjFxJyZmIUaAtiE%22%2C%22trigger_id%22%3A%22394317089889.353057346949.056f6f93eda933320496323fef821661%22%7D',
-             'isBase64Encoded': False}
+    event = {'resource': '/frichti', 'path': '/frichti', 'httpMethod': 'POST', 'headers': {'Accept': 'application/json,*/*', 'Accept-Encoding': 'gzip,deflate', 'CloudFront-Forwarded-Proto': 'https', 'CloudFront-Is-Desktop-Viewer': 'true', 'CloudFront-Is-Mobile-Viewer': 'false', 'CloudFront-Is-SmartTV-Viewer': 'false', 'CloudFront-Is-Tablet-Viewer': 'false', 'CloudFront-Viewer-Country': 'US', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'cwpc4sd08l.execute-api.us-east-1.amazonaws.com', 'User-Agent': 'Slackbot 1.0 (+https://api.slack.com/robots)', 'Via': '1.1 5cb08f1fdb064b6046c3b17fe92559f8.cloudfront.net (CloudFront)', 'X-Amz-Cf-Id': 'U2HEc-sfLbYtwwROmEDgQDwAjlRR7Ko1xxUHzCtA5zfsbLcXDAhZNw==', 'X-Amzn-Trace-Id': 'Root=1-5b4757a2-26eeff7d2f29278bf25bb815', 'X-Forwarded-For': '52.207.53.149, 54.182.230.59', 'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https', 'X-Slack-Request-Timestamp': '1531402146', 'X-Slack-Signature': 'v0=61d5f963a62c45d3f51c7e646f29406378b7dc74ffc4454411c55d00645cca07'}, 'queryStringParameters': None, 'pathParameters': None, 'stageVariables': None, 'requestContext': {'resourceId': '1n9979', 'resourcePath': '/frichti', 'httpMethod': 'POST', 'extendedRequestId': 'J6qhYGrYIAMF2xw=', 'requestTime': '12/Jul/2018:13:29:06 +0000', 'path': '/dev/frichti', 'accountId': '483879061606', 'protocol': 'HTTP/1.1', 'stage': 'dev', 'requestTimeEpoch': 1531402146491, 'requestId': '8d00c1a1-85d7-11e8-88f4-8d0e0298850e', 'identity': {'cognitoIdentityPoolId': None, 'accountId': None, 'cognitoIdentityId': None, 'caller': None, 'sourceIp': '52.207.53.149', 'accessKey': None, 'cognitoAuthenticationType': None, 'cognitoAuthenticationProvider': None, 'userArn': None, 'userAgent': 'Slackbot 1.0 (+https://api.slack.com/robots)', 'user': None}, 'apiId': 'cwpc4sd08l'}, 'body': 'payload=%7B%22type%22%3A%22interactive_message%22%2C%22actions%22%3A%5B%7B%22name%22%3A%22remove%22%2C%22type%22%3A%22button%22%2C%22value%22%3A%22%7B%5C%22plate_type%5C%22%3A+%5C%22starter%5C%22%2C+%5C%22plate%5C%22%3A+%5C%22poulet%5C%22%7D%22%7D%5D%2C%22callback_id%22%3A%22edit%22%2C%22team%22%3A%7B%22id%22%3A%22TAD1PA6TX%22%2C%22domain%22%3A%22pussycratie%22%7D%2C%22channel%22%3A%7B%22id%22%3A%22DADUGGRV5%22%2C%22name%22%3A%22directmessage%22%7D%2C%22user%22%3A%7B%22id%22%3A%22UADUGGKAB%22%2C%22name%22%3A%22stephane%22%7D%2C%22action_ts%22%3A%221531402146.440287%22%2C%22message_ts%22%3A%221531402143.000363%22%2C%22attachment_id%22%3A%221%22%2C%22token%22%3A%22j96WF0mOCsNFaPBninbGyUnX%22%2C%22is_app_unfurl%22%3Afalse%2C%22response_url%22%3A%22https%3A%5C%2F%5C%2Fhooks.slack.com%5C%2Factions%5C%2FTAD1PA6TX%5C%2F398679068118%5C%2FzxEH19p5eudM0d16rx6z0lDY%22%2C%22trigger_id%22%3A%22397457085090.353057346949.824172e835a38567ebbfacb98d1d5f7f%22%7D', 'isBase64Encoded': False}
 
     # print(handler(event,None))
 
     # remove()
-    # order('toto', 'stephane', 'main', 'orange', u'éclair au chocolat')
-    #handler(event, None)
-    print(json.dumps(list_user('U8N28JF1V',  text='Your order did not change')))
+    #order('UBHR8116Y', 'cyril.vinot', 'main', "Poulet fermier à l'orange & légumes verts")
+    handler(event, None)
+    #print(json.dumps(list_user('U8N28JF1V',  text='Your order did not change')))
